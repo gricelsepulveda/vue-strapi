7838a9a7fa3f668779a7dbb06a787d7c6fc24e81
Gricel <hello@gricel.dev>
2021-04-12 15:36:31 -0400

    Cambios

---
+++ src/TestComponent/TestComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Holi
+    </div>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue'
+  export default Vue.component('TestComponent', {
+    props: {
+        "data": String
+    }
+  })
+</script>
